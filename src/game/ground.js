dcffcf815fa00296ba88a2fc3b3b5b68d37b2be7
laguigzzz <guillaume.samson-richert@coda-student.school>
2023-12-04 15:19:02 +0100

    adding path to bg

--- src/game/ground.js
+++ src/game/ground.js
@@ -1,11 +1,15 @@
 // gestion du terrain
 function preloadWorld(gameloop) {
-    gameloop.load.atlas('tiles', 'assets/sets/platformer.png', 'assets/sets/platformer.json');
-    gameloop.load.image('bg', 'assets/sets/background.png');
+    gameloop.load.atlas('tiles', '/assets/platformer.png', 'assets/platformer.json');
+    gameloop.load.image('bg', '/assets/background.png');
 }
 
 function createWorld(gameloop) {
+    
+    gameloop.add.image(400, 300, 'bg');
+    
     const ground = gameloop.physics.add.staticGroup();
+    
     ground.create(64, 536, 'tiles', '2');
     ground.create(64+128, 536, 'tiles', '2');
     ground.create(64+256, 536, 'tiles', '2');
@@ -13,5 +17,5 @@ function createWorld(gameloop) {
     ground.create(64+512, 536, 'tiles', '2');
     ground.create(64+640, 536, 'tiles', '2');
     ground.create(64+768, 536, 'tiles', '2');
-    gameloop.add.image(400, 300, 'bg');
+    
 }
