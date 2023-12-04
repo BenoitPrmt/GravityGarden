5ca7a8aa2594ca3b3f3daef75cc39501edc47174
laguigzzz <guillaume.samson-richert@coda-student.school>
2023-12-04 15:10:27 +0100

    preload

--- src/game/ground.js
+++ src/game/ground.js
@@ -1,7 +1,17 @@
 // gestion du terrain
-function createWorld(gameloop) {
+function preloadWorld(gameloop) {
+    gameloop.load.atlas('tiles', 'assets/sets/platformer.png', 'assets/sets/platformer.json');
+    gameloop.load.image('bg', 'assets/sets/background.png');
+}
 
-    gameloop.platforms = gameloop.physics.add.staticGroup();
-    gameloop.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-    
+function createWorld(gameloop) {
+    const ground = gameloop.physics.add.staticGroup();
+    ground.create(64, 536, 'tiles', '2');
+    ground.create(64+128, 536, 'tiles', '2');
+    ground.create(64+256, 536, 'tiles', '2');
+    ground.create(64+384, 536, 'tiles', '2');
+    ground.create(64+512, 536, 'tiles', '2');
+    ground.create(64+640, 536, 'tiles', '2');
+    ground.create(64+768, 536, 'tiles', '2');
+    gameloop.add.image(400, 300, 'bg');
 }
