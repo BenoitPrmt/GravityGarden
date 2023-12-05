7ab6064da6b7a2ef94d5a8d28897e5f81800bc13
laguigzzz <guillaume.samson-richert@coda-student.school>
2023-12-05 11:06:24 +0100

    adding ground

--- src/game/ground.js
+++ src/game/ground.js
@@ -1,7 +1,8 @@
 // gestion du terrain
 function preloadWorld(gameloop) {
-    gameloop.load.atlas('tiles', '/assets/platformer.png', '/assets/platformer.json');
+    // gameloop.load.atlas('tiles', '/assets/platformer.png', '/assets/platformer.json');
     gameloop.load.image('bg', '/assets/gravitygarden.png');
+    gameloop.load.atlas('ground', '/assets/ground.png', '/assets/ground.json');
 }
 
 function createWorld(gameloop) {
@@ -10,15 +11,19 @@ function createWorld(gameloop) {
 
     const ground = gameloop.physics.add.staticGroup();
     
-    ground.create(64, 936, 'tiles', '2');
-    ground.create(64+128, 936, 'tiles', '2');
-    ground.create(64+256, 936, 'tiles', '2');
-    ground.create(64+384, 936, 'tiles', '2');
-    ground.create(64+512, 936, 'tiles', '2');
-    ground.create(64+640, 936, 'tiles', '2');
-    ground.create(64+768, 936, 'tiles', '2');
+    // ground.create(64, 936, 'tiles', '2');
+    // ground.create(64+128, 936, 'tiles', '2');
+    // ground.create(64+256, 936, 'tiles', '2');
+    // ground.create(64+384, 936, 'tiles', '2');
+    // ground.create(64+512, 936, 'tiles', '2');
+    // ground.create(64+640, 936, 'tiles', '2');
+    // ground.create(64+768, 936, 'tiles', '2');
 
-    ground.refresh();
+    ground.create(400, 955, 'ground', '1');
+
+    // .refreshBody()
+
+    // ground.refresh();
     
     // gameloop.physics.world.setBounds(0, -400, 800, 1000);
     
